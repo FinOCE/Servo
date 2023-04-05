869649fb370a3bb35f940b01c28c8a5060096e32
me@itsf.in <me@itsf.in>
2023-04-05 23:04:53 +1000

    Progress deciphering replays

---
+++ Servo/ReplayPreprocessor/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿namespace ReplayPreprocessor;
+
+public class Actor
+{
+    public uint Id;
+    
+    public string? Class;
+
+    public string? Object;
+
+    public string? Name;
+
+    public Vector3D? Position;
+
+    public Rotator? Rotation;
+}
--- Servo/ReplayPreprocessor/Program.cs
+++ Servo/ReplayPreprocessor/Program.cs
@@ -1,27 +1,29 @@
 ﻿// Get file name from console
-string? fileName = null;
-do
-{
-    Console.Write("Enter replay file name: ");
-    string? input = Console.ReadLine();
-    
-    if (input is not null && input != "")
-        fileName = input;
-}
-while (fileName is null);
+//string? fileName = null;
+//do
+//{
+//    Console.Write("Enter replay file name: ");
+//    string? input = Console.ReadLine();
+
+//    if (input is not null && input != "")
+//        fileName = input;
+//}
+//while (fileName is null);
+string fileName = "55542b99-566a-4f05-85e0-5b4be2c3934f";
 
 // Fetch replay file and handle if it doesn't exist
 Replay? replay = Parser.GetReplay(fileName);
 
 if (replay is null)
 {
-    Console.WriteLine($"Could not find Replay/{fileName}.replay");
+    Console.WriteLine($"Could not find the replay. Press enter to exit...");
     Console.Read();
     return;
 }
 
-// Parse replay file
-Parser.SaveJson(fileName, replay);
+// Test the replay
+Parser.Test(replay);
 
 // Await key press to close console
+Console.WriteLine("Task completed. Press enter to exit...");
 Console.Read();
